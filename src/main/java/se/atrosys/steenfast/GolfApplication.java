4dde079f7371a83e098a1d859388261ed10f317e
Ola Sundell <ola.sundell@gmail.com>
2016-04-05 09:32:01 +0200

    Initial commit

---
+++ src/main/java/se/atrosys/steenfast/GolfApplication.java
@@ -0,0 +1,12 @@
+package se.atrosys.steenfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfApplication.class, args);
+	}
+}
